d40911c95c17b32ee6280c016a61fe8225abe19b
python-tutoring-7657 <162918339+python-tutoring-7657@users.noreply.github.com>
2024-03-10 12:35:35 -0700

    Update example-loading-excel-sheet.py

--- example-loading-excel-sheet.py
+++ example-loading-excel-sheet.py
@@ -3,13 +3,8 @@ import pandas as pd
 # Define the location of the file name
 filename = 'C:/Users/Connie/Downloads/comparison.xlsx'
 
-# Open up the excel file
-xls = pd.ExcelFile(filename)
-
 # Pick the sheet name you want to load into a pandas dataframe
 df = pd.read_excel(filename, sheet_name="SheetNameGoesHere")
 
 # Print the first five rows that are loaded
 print(df.head(5))
-
-df_temp = df
